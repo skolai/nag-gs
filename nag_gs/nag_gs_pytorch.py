cc825d6ddd08e642477474ba6ac1d5f5d71103e4
Daniel Bershatsky <daniel.bershatsky@gmail.com>
2022-11-17 02:38:13 +0300

    Add full-step NAG-GS optimizer in PyTorch

--- nag_gs/nag_gs_pytorch.py
+++ nag_gs/nag_gs_pytorch.py
@@ -87,3 +87,89 @@ def nag4(params: List[T.Tensor], grads: List[T.Tensor],
         # Update parameters
         param.add_(v, alpha=alpha)
         param.div_(1+alpha)
+
+
+class NAGGS(T.optim.Optimizer):
+    """Class NAGGS implements algorithm with update on gamma which stands for
+    the semi-implicit integration of the Nesterov Accelerated Gradient (NAG)
+    flow.
+
+    Arguments
+    ---------
+        params (collection): Collection of parameters to optimize.
+        lr (float, optional): Learning rate (or alpha).
+        mu (float, optional): Momentum mu.
+        gamm (float, optional): Gamma factor.
+    """
+
+    def __init__(self, params, lr=1e-2, mu=1.0, gamma=1.0):
+        if lr < 0.0:
+            raise ValueError(f"alpha: {lr}")
+        if mu < 0.0:
+            raise ValueError(f"Invalid mu: {mu}")
+        if gamma < 0.0:
+            raise ValueError('Parameter gamma should be non-nevative.')
+
+        defaults = dict(lr=lr, mu=mu, gamma=gamma)
+        super().__init__(params, defaults)
+
+    @T.no_grad()
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments
+        ---------
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            with T.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            state = []
+            params = []
+            grads = []
+            gammas = []
+
+            for p in filter(lambda x: x is not None, group['params']):
+                if len(param_state := self.state[p]) == 0:
+                    param_state['state'] = T.clone(p).detach()
+                state.append(param_state['state'])
+                params.append(p)
+                grads.append(p.grad)
+                gammas.append(param_state.get('gamma', group['gamma']))
+
+            nag_gs(state, params, grads, group['lr'], gammas, group['mu'])
+
+            for param, gamma in zip(params, gammas):
+                self.state[param]['gamma'] = gamma
+
+        return loss
+
+
+def nag_gs(state: List[T.Tensor], params: List[T.Tensor],
+           grads: List[T.Tensor], alpha: float, gammas: List[float],
+           mu: float):
+    """Function nag-gs performs full NAG-GS algorithm computation.
+    """
+    gammas_out = []
+    for gamma, gs, xs, vs in zip(gammas, grads, params, state):
+        # 1. Update state: variable v.
+        b = alpha * mu / alpha * mu + gamma
+        vs.mul_(1 - b)
+        vs.add_(xs, alpha=b)
+        vs.add_(gs, alpha=-b / mu)
+
+        # 2. Update gamma coefficient.
+        gamma = (alpha * mu + gamma) / (1 + alpha)
+        gammas_out.append(gamma)
+
+        # 3. Update parameters: variable x.
+        #   x <- (1 - a) x  + a v
+        a = alpha / (alpha + 1)
+        xs.mul_(1 - a)
+        xs.add_(vs, alpha=a)
+
+    return gammas_out
--- nag_gs/nag_gs_pytorch_test.py
+++ nag_gs/nag_gs_pytorch_test.py
@@ -1,23 +1,25 @@
 import pytest
 import torch as T
 
-from nag_gs.nag_gs_pytorch import NAG4
+from nag_gs.nag_gs_pytorch import NAG4, NAGGS
 
 
 class TestNAGOptimizer:
 
-    def test_sanity(self):
+    @pytest.mark.parametrize('opt_ty', (NAG4, NAGGS))
+    def test_sanity(self, opt_ty):
         params = T.ones(8)
-        _ = NAG4([params])
+        _ = opt_ty([params])
 
-    def test_quadratic3d(self):
+    @pytest.mark.parametrize('opt_ty', (NAG4, NAGGS))
+    def test_quadratic3d(self, opt_ty):
 
         def fn(xs):
             return xs @ xs
 
         eps = 1e-3
         params = T.ones(3, requires_grad=True)
-        opt = NAG4([params], lr=0.05, mu=1.0, gamma=1.5)
+        opt = opt_ty([params], lr=0.05, mu=1.0, gamma=1.5)
         for _ in range(200):
             loss = fn(params)
             loss.backward()
